12f8cb5bf3e4cae6e40ae300222b2cad89dc344c
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-05-13 03:44:22 -0700

    05/13/2016 3:44 AM

---
+++ js/src/assert.js
@@ -0,0 +1,28 @@
+var isConstructor, steal, throwFailure;
+
+throwFailure = require("failure").throwFailure;
+
+isConstructor = require("isConstructor");
+
+steal = require("steal");
+
+module.exports = function(invariant, meta) {
+  var error, reason;
+  if (invariant) {
+    return;
+  }
+  if (isConstructor(meta, Function)) {
+    meta = meta();
+  }
+  if (isConstructor(meta, Object)) {
+    reason = steal(meta, "reason");
+  } else {
+    meta = {};
+  }
+  error = Error(reason || "Assertion failed.");
+  error.skip = meta.skip || 0;
+  error.skip += 2;
+  return throwFailure(error, meta);
+};
+
+//# sourceMappingURL=../../map/src/assert.map
---
+++ src/assert.coffee
@@ -0,0 +1,22 @@
+
+{ throwFailure } = require "failure"
+
+isConstructor = require "isConstructor"
+steal = require "steal"
+
+module.exports = (invariant, meta) ->
+
+  return if invariant
+
+  if isConstructor meta, Function
+    meta = meta()
+
+  if isConstructor meta, Object
+    reason = steal meta, "reason"
+  else meta = {}
+
+  error = Error reason or "Assertion failed."
+  error.skip = meta.skip or 0
+  error.skip += 2
+
+  throwFailure error, meta
